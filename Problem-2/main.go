8c6fde9a58a41d6016990449c77ef5da94de636c
Abhyudit Jain <abhyuditjain@gmail.com>
2019-08-20 03:07:46 +0530

    reorganised

--- Problem-2/main.go
+++ Problem-2/main.go
@@ -1,7 +1,3 @@
-package main
-
-import "fmt"
-
 /**
 Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
 
@@ -9,6 +5,9 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 
 Follow-up: what if you can't use division?
 */
+package main
+
+import "fmt"
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5}
--- Problem-3/main.go
+++ Problem-3/main.go
@@ -1,3 +1,9 @@
+/*
+This problem was asked by Google.
+
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+*/
+
 package main
 
 import (
